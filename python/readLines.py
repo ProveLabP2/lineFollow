21653f7dd7fb5a46655d0698a7303d27f753c1e0
Chris <cvaranes@calpoly.edu>
2018-01-24 18:23:01 -0800

    fixed config

--- python/readLines.py
+++ python/readLines.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import cv2
 import matplotlib
 import pyCV
@@ -9,17 +10,18 @@ import matplotlib.animation as animation
 
 
 def main(argv):
+    config = []
     if len(argv) > 1:
         config = configparser.ConfigParser()
         config.read(argv[1])
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     fig = plt.figure()
     ax = fig.add_subplot(111)
     im = ax.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), animated=True)
     def updatefig(*args):
         ret, frame = cap.read()
-        if len(argv) > 1:
+        if len(argv) == 1:
             frame = pyCV.line_image(frame)
         else:
             frame = pyCV.line_image(frame,
@@ -35,6 +37,6 @@ def main(argv):
     ani = animation.FuncAnimation(fig, updatefig, interval=1)
     plt.show()
 
-if __name__ == "main":
+if __name__ == "__main__":
     main(sys.argv)
 
