785109edf8b4a18449c5c4ea3998d4ce9d556915
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2018-10-08 16:34:35 -0700

    updated code

--- new_code/cvImpl.py
+++ new_code/cvImpl.py
@@ -1,6 +1,8 @@
 import cv2, time
 import numpy as np
 
+old_lines = []
+
 def get_camera_capture():
     '''
         Returns a camera capture object for OpenCV.
@@ -39,6 +41,23 @@ def end_program(list_of_captures):
     '''
     [capture.release() for capture in list_of_captures]
     cv2.destroyAllWindows()
+
+def find_average_line(lines):
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+
+    for line in lines:
+        x1 += line[0][0]
+        y1 += line[0][1] 
+        x2 += line[0][2]
+        y2 += line[0][3]
+
+    avg = [[[x1//(len(lines)), y1//(len(lines)), x2//(len(lines)), y2//(len(lines))]]]
+
+
+    return avg
 
 def grayscale(img):
     """Applies the Grayscale transform
@@ -82,7 +101,7 @@ def region_of_interest(img, vertices):
     return masked_image
 
 
-def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+def draw_lines(img, lines, color=[0, 0, 255], thickness=3):
     """
     NOTE: this is the function you might want to use as a starting point once you want to
     average/extrapolate the line segments you detect to map out the full
@@ -97,10 +116,16 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
     If you want to make the lines semi-transparent, think about combining
     this function with the weighted_img() function below
     """
-    if lines is None:
-        return 
+    global old_lines
+
+    if lines is None or len(lines) < 2:
+        lines = old_lines
+
+    else:
+        old_lines = lines
 
     for line in lines:
+
         for x1,y1,x2,y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
@@ -129,17 +154,17 @@ def weighted_img(img, initial_img, a=0.8, b=1., l=0.):
 
 
 def process_image(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_image = gaussian_blur(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 9)
     img_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     lower_yellow = np.array([20, 100, 100], dtype = "uint8")
-    upper_yellow = np.array([30, 255, 255], dtype="uint8")
+    upper_yellow = np.array([160, 255, 255], dtype="uint8")
 
     mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
-    mask_white = cv2.inRange(gray_image, 200, 255)
+    mask_white = cv2.inRange(gray_image, 120, 255)
     mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
     mask_yw_image = cv2.bitwise_and(gray_image, mask_yw)
 
-    kernel_size = 5
+    kernel_size = 9
     gauss_gray = gaussian_blur(mask_yw_image,kernel_size)
     #same as quiz values
     low_threshold = 50
@@ -147,10 +172,10 @@ def process_image(image):
     canny_edges = canny(gauss_gray,low_threshold,high_threshold)
 
     imshape = image.shape
-    lower_left = [imshape[1]/9,imshape[0]]
-    lower_right = [imshape[1]-imshape[1]/9,imshape[0]]
-    top_left = [imshape[1]/2-imshape[1]/8,imshape[0]/2+imshape[0]/10]
-    top_right = [imshape[1]/2+imshape[1]/8,imshape[0]/2+imshape[0]/10]
+    lower_left = [0,imshape[0]]
+    lower_right = [imshape[1],imshape[0]]
+    top_left = [0,imshape[0]/2+imshape[0]/8]
+    top_right = [imshape[1],imshape[0]/2+imshape[0]/8]
     vertices = [np.array([lower_left,top_left,top_right,lower_right],dtype=np.int32)]
     roi_image = region_of_interest(canny_edges, vertices)
 
@@ -159,14 +184,13 @@ def process_image(image):
     rho = 2
     theta = np.pi/180
     #threshold is minimum number of intersections in a grid for candidate line to go to output
-    threshold = 20
+    threshold = 40
     min_line_len = 50
     max_line_gap = 200
 
     line_image = hough_lines(roi_image, rho, theta, threshold, min_line_len, max_line_gap)
     result = weighted_img(line_image, image, a=0.8, b=1., l=0.)
-    return roi_image
-
+    return result
 
 '''
 Sample code:
@@ -174,13 +198,13 @@ Sample code:
 
 create_window("window_name")
 #capture = get_camera_capture()
-capture = get_video_capture("vid.mp4")
+capture = get_video_capture("vid1.mp4")
 while True:
     k = cv2.waitKey(1)
     if k==27:
         break
     image = capture.read()[1]
-    display_to_window("window_name", process_image(image))
+    display_to_window("window_name", process_image(cv2.resize(image, (500, 500))))
 
 end_program([capture])
 
